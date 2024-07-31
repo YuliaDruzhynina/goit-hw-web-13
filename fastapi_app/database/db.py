0f99bf708d6c954217a6ae6cb207b9d0f708ea88
Poshtaklon <152329594+YuliaDruzhynina@users.noreply.github.com>
2024-07-31 21:41:20 +0300

    Update db.py

--- fastapi_app/database/db.py
+++ fastapi_app/database/db.py
@@ -28,3 +28,25 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# pip install asyncpg
+# pip install sqlalchemy[asyncio]
+# from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+# from sqlalchemy.orm import sessionmaker
+# from src.conf.config import settings
+
+
+#engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+
+# SessionLocal = sessionmaker(
+#     bind=engine,
+#     class_=AsyncSession,
+#     expire_on_commit=False,
+#     autocommit=False,
+#     autoflush=False,
+# )
+
+# async def get_db():
+#     async with SessionLocal() as session:
+#         yield session
